583529eaf5fe7bf765fb6bcddf3fdbd90de9111e
Guido Nuñez <dreamguido@gmail.com>
2022-02-12 16:22:47 -0500

    primera subida de codigo para uso

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # img2video
+
+Create video from images
+version: 0.2
+autor: guidonun3z
+
+**La instrucciones de uso basico son<br>**
+
+1. Clonar el repositorio para tener una copia local:<br>
+   ` $git clone https://github.com/guidonun3z/img2video`<br>
+2. Agregar la libreria OpenCV<br>
+   ` $conda install opencv` ó ` $pip install opencv`<br>
+3. Llenar los datos
+   `hhini=000 `hora de inicio en formato hhmm<br>
+   `hhfin=0000 `hora de fin en formato hhmm<br>
+   `dia="0000" `dia y mes en formato ddmm<br>
+   `camara="nombre"`npombre de la camara<br>
+   `ext=".avi"`extension del video <br>
---
+++ img2video.py
@@ -0,0 +1,57 @@
+#create video from images
+#version: 0.2
+# autor: GENA
+
+from fileinput import filename
+import cv2
+import os
+from os.path import isfile, join
+from numpy import *
+import random
+
+nrad=random.randint(0,999)
+# video especifications 
+
+hhini=000
+hhfin=2400
+dia="0211"
+camara="TSOQ22"
+ext=".avi"
+
+
+def convert_pictures_to_video(pathIn, pathOut, fps, time):
+    '''this function converts pictures to video'''
+    frame_array = []
+    files=[f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]    
+    files2=[j for j in files if hhini<=int(j[-10:-6])<=hhfin ]
+ 
+    for i in range (len(files2)):
+        filename=pathIn+files2[i]
+        ''''readin images'''
+        img=cv2.imread(filename)
+        #change size
+        #img=cv2.resize(img,(1400,1600))
+        
+        height, width, layers = img.shape
+        size=(width, height)
+        
+        for k in range (time):
+            frame_array.append(img)
+
+    out=cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'mp4v'), fps,size)
+
+    for i in range(len(frame_array)):
+        out.write(frame_array[i])
+    out.release()
+    print("se creó el video exitosamente\n","se compilaron " + str(len(files2))+ " archivos" )
+    
+#out file configuration: 
+directory='J:/IngemmetWork/monitoreo/Camaras/Ticsani/TSOQ/2022/02/11'
+pathIn=directory+'/'
+pathOut=pathIn+camara+'-'+str(hhini)+'-'+str(hhfin)+'-'+dia+'-'+str(nrad)+ext
+fps=25
+time=5 #time duration of each pictures
+
+#calling function
+convert_pictures_to_video(pathIn, pathOut, fps, time)
+
